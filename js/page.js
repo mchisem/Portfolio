04723231fd6416791e33f64dcbb6417b5c1f60f9
Maya Chisem <chisem.maya@gmail.com>
2020-06-25 20:11:36 -0700

    update

--- js/page.js
+++ js/page.js
@@ -6,21 +6,28 @@
          const title = section.querySelector("h1");
          const tl = new TimelineMax({delay: 0.5});
          const video = document.querySelector("#watch");
+         const resume = document.querySelector("#file");
          const img1 = document.querySelector("#img1");
+         const img2 = document.querySelector("#one");
 
          tl.fromTo(title, 0.5, 
             {y: "100", opacity: 0}, {y: 0, opacity: 1});
         
         if(destination.index === 0) {
+            video.classList.remove("hide");
+
             tl.fromTo(video, 0.5,
                 {y: 0, opacity: 0}, {y: 0, opacity: 1}
                 );
 
-            video.classList.remove("hide");
         }
 
         else if(destination.index === 1) {
             video.classList.add("hide");
+
+            tl.fromTo(resume, 0.5,
+                {x: "100", opacity: 0}, {x: 0, opacity: 1} 
+                 );
 
         }  
 
@@ -34,6 +41,10 @@
  
         else if(destination.index === 3) {
             video.classList.add("hide");
+
+            tl.fromTo(img2, 0.5,
+                {x: "100", opacity: 0}, {x: 0, opacity: 1} 
+                 );
         }
 
         else if(destination.index === 4) {
