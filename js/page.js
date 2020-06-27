b55b96570d3466d6194dc6778e510904d4b5aa97
Maya Chisem <chisem.maya@gmail.com>
2020-06-27 15:09:37 -0700

    update

--- js/page.js
+++ js/page.js
@@ -1,6 +1,6 @@
  new fullpage("#fullpage", {
      autoScrolling: true,
-     navigation: false,
+     navigation: true,
      onLeave: (origin, destination, direction) => {
          const section = destination.item;
          const title = section.querySelector("h1");
@@ -9,20 +9,25 @@
          const resume = document.querySelector("#file");
          const img1 = document.querySelector("#img1");
          const img2 = document.querySelector("#one");
-         const arrow = document.querySelector("#arrow");
 
          tl.fromTo(title, 0.5, 
             {y: "100", opacity: 0}, {y: 0, opacity: 1});
         
         if(destination.index === 0) {
-            video.classList.remove("hide");
+            document.getElementById("home").style.color = "black";
+            document.getElementById("contact").style.color = "black";
 
+            video.classList.remove("hide");
+          
             tl.fromTo(video, 0.5,
                 {y: 0, opacity: 0}, {y: 0, opacity: 1}
                 );
         }
 
         else if(destination.index === 1) {
+            document.getElementById("home").style.color = "white";
+            document.getElementById("contact").style.color = "white";
+
             video.classList.add("hide");
 
             tl.fromTo(resume, 0.5,
@@ -31,14 +36,21 @@
         }  
 
         else if(destination.index === 2) {
+            document.getElementById("contact").style.color = "black";
+            document.getElementById("home").style.color = "black";
+
             video.classList.add("hide");
 
             tl.fromTo(img1, 0.5,
                 {x: "-100", opacity: 0}, {x: 0, opacity: 1} 
                  );
+
         }
  
         else if(destination.index === 3) {
+            document.getElementById("contact").style.color = "black";
+            document.getElementById("home").style.color = "black";
+
             video.classList.add("hide");
 
             tl.fromTo(img2, 0.5,
